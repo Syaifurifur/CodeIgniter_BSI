c93273ab6b5afa7f16d8de9bd3fb1a61cd6ccfe9
Syaifurifur <syaifur.syl@bsi.ac.id>
2022-04-07 05:34:32 +0700

    templating web sederhana

---
+++ application/views/v_about.php
@@ -0,0 +1,27 @@
+<section>
+<h1>
+    <?php echo $judul ?>
+</h1>
+<h4>
+<h4>Nama</h4>
+<ul type="disc">
+<li>Nama Depan : Syaifur</li>
+<li>Nama Belakang : Rahmatullah</li>
+</ul>
+<br>
+<h4>Alamat</h4>
+<ul type="none">
+<li> Jalan Legok, Rancagong, Kabupaten Tangerang</li>
+</ul>
+<h4>Tempat Lahir</h4>
+<ul type="none">
+<li>Jakart</li>
+</ul>
+<h4>Olah Raga Favorit</h4>
+<ul type="square">
+<li>Bulutangkis</li>
+<li>Sepeda</li>
+</ul>
+</h4>
+
+</section>
